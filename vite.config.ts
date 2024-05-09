28855a82f94f59cc61a6a0b5689ada19608a8ce2
hikaruonitsuka <linlin.work071109@gmail.com>
2024-05-09 11:15:35 +0900

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/react-gh-pages/",
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+  },
+  plugins: [tsconfigPaths(), react()],
+});
